12180c4a1b2a4693582a3fa96f8ef99b8d520242
Artem Anikieiev <artem@trackonomysystems.com>
2023-02-14 15:49:35 -0800

    cicd workflow develop

--- terraform/node-dev/startup.sh
+++ terraform/node-dev/startup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 echo "Starting apps"
-cd /apis/node-dev
-pm2 start filter/bin/www --name filter
-pm2 start mobile/bin/www --name mobile
-pm2 start util/bin/www --name util
-pm2 start external/bin/www --name external
-pm2 start health-dash/bin/www --name health-dash
-pm2 start tapeevents/bin/www --name tapeevents
-pm2 save
-pm2 startup
-pm2 save
+su - azureadm -c "cd /apis/node-dev && pm2 start filter/bin/www --name filter && 
+pm2 start mobile/bin/www --name mobile && 
+pm2 start util/bin/www --name util && 
+pm2 start external/bin/www --name external && 
+pm2 start health-dash/bin/www --name health-dash && 
+pm2 start tapeevents/bin/www --name tapeevents && 
+pm2 save && 
+pm2 startup && 
+pm2 save" 
 exit 0
