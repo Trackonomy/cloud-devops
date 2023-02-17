3e71f08fed5c590cc4d13f2094009152cfa6d7ff
Artem Anikieiev <artem@trackonomysystems.com>
2023-02-17 12:44:16 -0800

    Added reference to centos image

--- terraform/node-dev/startup.sh
+++ terraform/node-dev/startup.sh
@@ -8,6 +8,30 @@ pm2 start external/bin/www --name external &&
 pm2 start health-dash/bin/www --name health-dash && 
 pm2 start tapeevents/bin/www --name tapeevents && 
 pm2 save && 
-eval '$(pm2 startup | sed -n 3p)' && 
+tee -a /etc/systemd/system/pm2-azureadm.service <<EOF
+[Unit]
+Description=PM2 process manager
+Documentation=https://pm2.keymetrics.io/
+After=network.target
+
+[Service]
+Type=forking
+User=azureadm
+LimitNOFILE=infinity
+LimitNPROC=infinity
+LimitCORE=infinity
+Environment=PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin
+Environment=PM2_HOME=/home/azureadm/.pm2
+PIDFile=/home/azureadm/.pm2/pm2.pid
+Restart=on-failure
+
+ExecStart=/usr/lib/node_modules/pm2/bin/pm2 resurrect
+ExecReload=/usr/lib/node_modules/pm2/bin/pm2 reload all
+ExecStop=/usr/lib/node_modules/pm2/bin/pm2 kill
+
+[Install]
+WantedBy=multi-user.target
+EOF &&
+systemctl enable pm2-azureadm.service && 
 pm2 save" 
 exit 0
