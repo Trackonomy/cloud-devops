e61f1baa066be1bd00478b70ee57a85b6fe9d4b8
Artem Anikieiev <artem@trackonomysystems.com>
2023-03-10 16:46:32 -0800

    Added parametrization of startup script

--- terraform/node-dev/scaleset/startup.sh
+++ terraform/node-dev/scaleset/startup.sh
@@ -2,7 +2,7 @@
 
 start_all() {
   save_startup_file() {
-    sudo tee -a /etc/systemd/system/pm2-prdazureadmin.service <<- EOF
+    sudo tee -a /etc/systemd/system/pm2-{username}.service <<- EOF
 [Unit]
 Description=PM2 process manager
 Documentation=https://pm2.keymetrics.io/
@@ -10,13 +10,13 @@ After=network.target
 
 [Service]
 Type=forking
-User=prdazureadmin
+User={username}
 LimitNOFILE=infinity
 LimitNPROC=infinity
 LimitCORE=infinity
 Environment=PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin
-Environment=PM2_HOME=/home/prdazureadmin/.pm2
-PIDFile=/home/prdazureadmin/.pm2/pm2.pid
+Environment=PM2_HOME=/home/{username}/.pm2
+PIDFile=/home/{username}/.pm2/pm2.pid
 Restart=on-failure
 
 ExecStart=/usr/lib/node_modules/pm2/bin/pm2 resurrect
@@ -28,19 +28,19 @@ WantedBy=multi-user.target
 EOF
 }
 
-cd /apis/node-dev && pm2 start filter/bin/www --name filter -i max
-pm2 start mobile/bin/www --name mobile
-pm2 start util/bin/www --name util
-pm2 start external/bin/www --name external
-pm2 start health-dash/bin/www --name health-dash
-pm2 start tapeevents/bin/www --name tapeevents
+cd /apis/node-dev && pm2 start filter/bin/www --name filter {filter.params}
+pm2 start mobile/bin/www --name mobile {mobile.params}
+pm2 start util/bin/www --name util {util.params}
+pm2 start external/bin/www --name external {external.params}
+pm2 start health-dash/bin/www --name health-dash {health-dash.params}
+pm2 start tapeevents/bin/www --name tapeevents {tapeevents.params}
 pm2 save
 save_startup_file
-sudo systemctl enable pm2-prdazureadmin.service
+sudo systemctl enable pm2-{username}.service
 pm2 save
 }
 export -f start_all
 echo "Starting apps"
 
-su prdazureadmin -c "bash -c start_all" 
+su {username} -c "bash -c start_all" 
 exit 0
