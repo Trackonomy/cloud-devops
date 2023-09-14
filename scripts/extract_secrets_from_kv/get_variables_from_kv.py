4ed3e9a2258bc5d0320abc525074384b3514e00f
ArtemAnikieiev <artem@trackonomysystems.com>
2023-09-14 14:19:18 -0700

    added error handling

--- scripts/extract_secrets_from_kv/get_variables_from_kv.py
+++ scripts/extract_secrets_from_kv/get_variables_from_kv.py
@@ -5,6 +5,7 @@ import sys
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
 from secrets_names import get_secret_names
+from azure.core.exceptions import ResourceNotFoundError
 def get_kv_client():
     keyVaultName = os.environ["KEY_VAULT_NAME"]
     KVUri = f"https://{keyVaultName}.vault.azure.net"
@@ -25,7 +26,13 @@ def main():
     with open("variables.env", "w") as f:
         secret_names = get_secret_names(env)
         for secret in secret_names:
-            value = client.get_secret(secret).value
+            value = ''
+            try:
+                extracted_secret = client.get_secret(secret)
+                value = extracted_secret.value
+            except ResourceNotFoundError:
+                print(f"CANNOT EXTRACT SECRET {secret}")
+                sys.exit(1)
             secret_transformed = secret.replace("-", "_")
             f.write(f"{secret_transformed}={value}"  + "\n")
             print(f"Extracted {secret_transformed}")
